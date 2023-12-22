11d48939bf65d09a69d8e16c0135222b70a21e07
Diego Fernandez Duran <diego@goedi.net>
2023-12-22 22:03:49 +0100

    feat: add Cloudflare support

--- dnsupdater.py
+++ dnsupdater.py
@@ -1,61 +1,67 @@
 #!/usr/bin/env python3
 
 import argparse
-import boto3
 import logging
-import requests
+
+import ip
+import route53 as r53
+import cloudflare as cf
 
 
-logging.basicConfig(level=logging.ERROR)
+def route53_command(args):
+    ip_address = ip.get()
+    if not ip_address:
+        logging.error('IP cannot be retrieved.')
+        return
 
-def get_ip():
-    r = requests.get('http://ifconfig.co/ip')
-    if r.status_code == 200:
-        return r.text.strip()
+    if r53.update_dns(
+        hosted_zone=args.hosted_zone_id,
+        name=args.name,
+        ip=ip_address
+    ):
+        logging.info(f"IP on {args.hosted_zone_id} for {args.name} updated to {ip_address}.")
     else:
-        return None
+        logging.info(f"IP on {args.hosted_zone_id} for {args.name} failed to update to {ip_address}.")
+        
 
-def update_dns(hosted_zone, name, ip):
-    client = boto3.client('route53')
+def cloudflare_command(args):
+    ip_address = ip.get()
+    if not ip_address:
+        logging.error('IP cannot be retrieved.')
+        return
 
-    response = client.change_resource_record_sets(
-        HostedZoneId=hosted_zone,
-        ChangeBatch={
-            "Comment": "Automatic record update",
-            "Changes": [{
-                "Action": "UPSERT",
-                "ResourceRecordSet": {
-                    "Name": name,
-                    "Type": "A",
-                    "TTL": 300,
-                    "ResourceRecords": [{
-                        "Value": ip
-                    }]
-                }
-            }]
-        }
-    )
-
-    logging.debug(response)
+    if cf.update_dns(
+        domain=args.domain,
+        name=args.name,
+        ip=ip_address
+    ):
+        logging.info(f"IP on {args.domain} for {args.name} updated to {ip_address}.")
+    else:
+        logging.info(f"IP on {args.domain} for {args.name} failed to update to {ip_address}.")
 
 def main():
     parser = argparse.ArgumentParser(
-        description="DNSupdater: Update an AWS Hosted Zone with your current IP"
+        description="DNSupdater: Update an AWS R53 Hosted Zone or Cloudflare domain with your current IP"
     )
-    parser.add_argument("--hosted-zone-id", type=str, required=True, help="Hosted Zone ID")
-    parser.add_argument("--name", type=str, required=True, help="Record name")
+    parser.add_argument("-l", "--log", dest="logLevel", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help="Set the logging level", default="INFO")
+
+    parser.add_argument("--name", type=str, required=True, help="record name")
+    subparsers = parser.add_subparsers(required=True, title='subcommands',
+                                   help='select DNS provider')
+    parser_r53 = subparsers.add_parser("route53", aliases=['r53'], help="R53 provider")
+    parser_r53.set_defaults(func=route53_command)
+    parser_r53.add_argument("--hosted-zone-id", type=str, required=True, help="hosted zone ID")
+
+    parser_cloudflare = subparsers.add_parser("cloudflare", aliases=['cf'], help="Cloudflare provider")
+    parser_cloudflare.add_argument("--domain", type=str, required=True, help="CF Domain")
+    parser_cloudflare.set_defaults(func=cloudflare_command)
+
     args = parser.parse_args()
 
-    ip = get_ip()
-    if ip:
-        update_dns(
-            hosted_zone=args.hosted_zone_id,
-            name=args.name,
-            ip=ip
-        )
-        print(f"IP on {args.hosted_zone_id} for {args.name} updated to {ip}.")
-    else:
-        logging.error('IP cannot be retrieved.')
+    logging.basicConfig(level=args.logLevel)
+
+    args.func(args)
 
 if __name__== "__main__":
   main()
